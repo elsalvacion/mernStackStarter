805083084bb2e5f8a3c30d400b1faed61298e518
elsalvacion <elsalvacion2022@gmail.com>
2021-09-23 01:42:13 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+GTTI app
+# mernStackStarter
---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const mongoSanitize = require("express-mongo-sanitize");
+const helmet = require("helmet");
+const xss = require("xss-clean");
+const hpp = require("hpp");
+
+// config
+const connectDB = require("./config/db");
+
+// require and use config files
+require("dotenv").config({ path: "./config/.env" });
+// connectDB();
+
+const app = express();
+
+// external middlewares
+app.use(mongoSanitize());
+app.use(helmet());
+app.use(xss());
+app.use(hpp());
+
+// express body parser
+
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+// listen to a port
+app.listen(PORT, () =>
+  console.log(
+    `server up and running on ${process.env.NODE_ENV} MODE at ${PORT}`
+  )
+);
